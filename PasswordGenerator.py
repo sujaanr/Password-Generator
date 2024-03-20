20b724c042c6e57b96c5a3240f09b110e3396ffd
Sujaan R <145608695+sujaanr@users.noreply.github.com>
2024-03-20 07:13:35 -0400

    Update PasswordGenerator.py

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,24 +1,29 @@
 import random
 import string
 
-# A function to shuffle all the characters of a string
-def shuffle(string):
-    tempList = list(string)
-    random.shuffle(tempList)
-    return ''.join(tempList)
+# A pure function to shuffle all the characters of a string and return a new string
+def shuffle_str(s):
+    tempList = list(s)
+    shuffledList = random.sample(tempList, len(tempList))
+    return ''.join(shuffledList)
 
-# Main program starts here
+# A function to generate a random digits string of a given length
+def generate_random_digits(length):
+    return ''.join(random.choice(string.digits) for _ in range(length))
 
-# Generate 10 digits
-digits = ''.join(random.choice(string.digits) for _ in range(10))
+# A function to generate a password of a given length
+def generate_password(length):
+    digits = generate_random_digits(length)
+    return shuffle_str(digits)
 
-# Generate password using all the digits, in random order
-length = int(input("Enter the password length: "))
-password = ''.join(random.choice(string.digits) for _ in range(length))
+# Function to save password to a file
+def save_password_to_file(password, filename):
+    with open(filename, 'w') as file:
+        file.write(password + '\n')
 
-# Output
-print(password)
-
-# Save to an output file
-with open('passwords.txt', 'w') as file:
-    file.write('\n'.join(password))
+# Main program
+if __name__ == "__main__":
+    length = int(input("Enter the password length: "))
+    password = generate_password(length)
+    print(password)
+    save_password_to_file(password, 'passwords.txt')
